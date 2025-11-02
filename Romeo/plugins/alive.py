200dc009a5528b8f9ea9318006ab1c7de3000b83
all456123 <allua456123@outlook.com>
2025-11-02 22:35:52 +0530

    fix

--- Romeo/plugins/alive.py
+++ Romeo/plugins/alive.py
@@ -29,7 +29,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-@Client.on_message(filters.command(["alive"], ".") & (filters.me | filters.user(SUDO_USER)))
+@Client.on_message(filters.command(["alive"], "."))
 async def alive(client: Client, message: Message):
     start = time()
     current_time = datetime.utcnow()
@@ -48,7 +48,7 @@ async def alive(client: Client, message: Message):
     await message.delete()
     await message.reply_photo(photo=ALIVE_PIC, caption=txt)
 
-@Client.on_message(filters.command(["ping"], ".") & (filters.me | filters.user(SUDO_USER)))
+@Client.on_message(filters.command(["ping"], "."))
 async def ping(client: Client, message: Message):
     r = await message.reply_text("**🇵𝐎𝐍𝐆**")
     start = time()
